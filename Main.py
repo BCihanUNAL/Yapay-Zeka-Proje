9df09b8faca9570770ffe4cf02ed7e10ded069b5
cihan43 <cihan_unal@outlook.com>
2020-05-08 15:13:41 +0300

    Bazı Hatalar Düzeltildi

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import pygame, sys
 import random
+import time
 
+speed_ball = 10
+speed_player = 7
 class Ball():
 
     def __init__(self,left,top,width,height,speed_x = 0,speed_y = 0,x = -1,y = -1):
@@ -18,23 +21,23 @@ class Ball():
         if y != -1 :
             self.obj.y = y
 
-    def animation(self, player, opponent, screen_width, screen_height):
+    def animation(self):
         self.obj.x += self.speed_x
         self.obj.y += self.speed_y
 
         if self.obj.top <= 0 or self.obj.bottom >= screen_height:
             self.speed_y *= -1
         if self.obj.left <= 0 or self.obj.right >= screen_width:
-            self.restart(screen_width,screen_height)
+            self.restart()
 
         if self.obj.colliderect(player):
             self.speed_x = abs(self.speed_x)
         if self.obj.colliderect(opponent):
             self.speed_x = -abs(self.speed_x)
 
-    def restart(self, screen_height, screen_width):
-        self.obj.centerx = screen_width/2
-        self.obj.centery = screen_height/2
+    def restart(self):
+        self.obj.left = screen_width/2 - 15
+        self.obj.top = screen_height/2 - 15
         self.speed_y *= random.choice((1, -1))
         self.speed_x *= random.choice((1, -1))
 
@@ -53,23 +56,27 @@ def calculate_state_value(ball_y,stick_y_down,stick_y_up):
     else:
         return -1.0
 
-def calculate_prize(ball_x,ball_y,stick_x,stick_y_down,stick_y_up):
+def calculate_prize(ball_x,ball_y,stick_x,stick_y_down,stick_y_up,speed_y):
     if ball_x + 15 >= stick_x:
         if int(ball_y + 15) < stick_y_down and (ball_y - 15) > stick_y_up:#ball_y in [stick_y_up - 15,stick_y_down + 15]:
             return 5.0
         else:
             return -5.0
     else:
-        if int(ball_y + 15) < stick_y_down and (ball_y - 15) > stick_y_up:
-            return 2.0
-        else:
-            return -1.0
+        #if int(ball_y + 15) < stick_y_down and (ball_y - 15) > stick_y_up:
+        #    return 2.0
+        #else:
+        dir_prize = 0
+        if speed_y < 0 and (stick_y_down+stick_y_up)/2 - ball_y < 0 or speed_y > 0 and (stick_y_down+stick_y_up)/2 - ball_y > 0 :
+            dir_prize = 0.5
+
+        return dir_prize - abs((stick_y_down+stick_y_up)/2 - ball_y)/screen_height
 
 
 
-def calculate(ball_x,ball_y,stick_x,stick_y_down,stick_y_up,alpha,decay_greedy,depth,speed_x,speed_y,depth_limit=2):
-    if depth > depth_limit or ball_x >= screen_width - 25:
-        return calculate_state_value(ball_y,stick_y_down,stick_y_up) + alpha * calculate_prize(ball_x,ball_y,stick_x,stick_y_down,stick_y_up)
+def calculate(ball_x,ball_y,stick_x,stick_y_down,stick_y_up,alpha,decay_greedy,depth,speed_x,speed_y,depth_limit=9):
+    if depth >= depth_limit or ball_x >= screen_width - 25:
+        return calculate_state_value(ball_y,stick_y_down,stick_y_up) + alpha * calculate_prize(ball_x,ball_y,stick_x,stick_y_down,stick_y_up,speed_y)
     else:
         s_x = speed_x
         s_y = speed_y
@@ -77,14 +84,15 @@ def calculate(ball_x,ball_y,stick_x,stick_y_down,stick_y_up,alpha,decay_greedy,d
             speed_y *= -1
         if ball_x+speed_x-15 <= 15: # oyuncu cubugunun sag tarafi
             speed_x *= -1
-        if stick_y_up - 7 < 0:
+        if stick_y_up - speed_player < 0:
             up_move_value = calculate(ball_x+s_x,ball_y+s_y,stick_x,stick_y_down-stick_y_up, 0, alpha, decay_greedy, depth + 1, speed_x, speed_y)
         else:
-            up_move_value = calculate(ball_x + s_x, ball_y + s_y, stick_x, stick_y_down - 7, stick_y_up - 7, alpha, decay_greedy, depth + 1, speed_x, speed_y)
-        if stick_y_down + 7 > screen_height:
+            up_move_value = calculate(ball_x + s_x, ball_y + s_y, stick_x, stick_y_down - speed_player, stick_y_up - speed_player, alpha, decay_greedy, depth + 1, speed_x, speed_y)
+
+        if stick_y_down + speed_player > screen_height:
             down_move_value = calculate(ball_x + s_x, ball_y + s_y, stick_x, screen_height, screen_height - (stick_y_down-stick_y_up), alpha, decay_greedy, depth + 1, speed_x, speed_y)
         else:
-            down_move_value = calculate(ball_x+s_x,ball_y+s_y,stick_x,stick_y_down + 7, stick_y_up + 7, alpha, decay_greedy, depth + 1, speed_x, speed_y)
+            down_move_value = calculate(ball_x+s_x,ball_y+s_y,stick_x,stick_y_down + speed_player, stick_y_up + speed_player, alpha, decay_greedy, depth + 1, speed_x, speed_y)
 
         return calculate_state_value(ball_y,stick_y_down,stick_y_up) + alpha * (decay_greedy * max(up_move_value,down_move_value) - calculate_state_value(ball_y,stick_y_down,stick_y_up))
 
@@ -113,7 +121,7 @@ screen_height = 768
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Pong')
 
-ball = Ball(screen_width/2 - 15, screen_height/2 - 15, 30, 30, 7, 7)
+ball = Ball(screen_width/2 - 15, screen_height/2 - 15, 30, 30, speed_ball, speed_ball)
 player = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 
@@ -132,29 +140,31 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                player_speed += 7
+                player_speed += speed_player
             if event.key == pygame.K_UP:
-                player_speed -= 7
+                player_speed -= speed_player
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
-                player_speed -= 7
+                player_speed -= speed_player
             if event.key == pygame.K_UP:
-                player_speed += 7
+                player_speed += speed_player
 
-    ball.animation(player,opponent,screen_width,screen_height)
+    ball.animation()
     player_animation()
 
-    up_move = calculate(ball.obj.left + (ball.obj.width / 2),ball.obj.top + (ball.obj.height / 2),opponent.left,opponent.bottom - 7,opponent.top - 7,alpha,decay_greedy,ball.speed_x,ball.speed_y,0)
-    down_move = calculate(ball.obj.left + (ball.obj.width / 2),ball.obj.top + (ball.obj.height / 2), opponent.left, opponent.bottom + 7, opponent.top + 7, alpha, decay_greedy,ball.speed_x,ball.speed_y,0)
 
-    print(up_move)
-    print(down_move)
-    print("--------------")
+    up_move = calculate(ball.obj.left + (ball.obj.width / 2),ball.obj.top + (ball.obj.height / 2),opponent.left,opponent.bottom - speed_player,opponent.top - speed_player,alpha,decay_greedy,0,ball.speed_x,ball.speed_y)
+    down_move = calculate(ball.obj.left + (ball.obj.width / 2),ball.obj.top + (ball.obj.height / 2), opponent.left, opponent.bottom + speed_player, opponent.top + speed_player, alpha, decay_greedy,0,ball.speed_x,ball.speed_y)
+
+
+    #print(up_move)
+    #print(down_move)
+    #print("--------------")
 
     if up_move >= down_move:
-        opponent_speed = -7
+        opponent_speed = -speed_player
     else:
-        opponent_speed = 7
+        opponent_speed = speed_player
 
     opponent_animation()
 
